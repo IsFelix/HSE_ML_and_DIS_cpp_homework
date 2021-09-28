72adc45895d0d31c546234dd060917b60b3da987
IsFelix <felix.isaakyan@gmail.com>
2021-09-28 19:46:56 +0300

    2nd homework

---
+++ cmake-build-debug/CMakeFiles/task_2A.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_2A.dir/yandex_contest_hw_2/task_2A.cpp.obj"
+  "libtask_2A.dll.a"
+  "task_2A.exe"
+  "task_2A.exe.manifest"
+  "task_2A.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_2A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
